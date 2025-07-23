a9387807701425ecfa436fa71068c5651296c3c9
Joseph Khella <josephkhella2010@gmail.com>
2025-07-23 20:52:36 +0200

    add product function

--- frontend/clothes/src/pages/Product/ProductPage.tsx
+++ frontend/clothes/src/pages/Product/ProductPage.tsx
@@ -1,3 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function ProductPage() {
-  return <div>ProductPage</div>;
+  const [productsData, setProductsData] = useState([]);
+  async function fetchProduct() {
+    try {
+      const response = await axios.get(
+        "https://cyrbaxdwxy.us-east-1.awsapprunner.com/api/products"
+      );
+      const { products } = response.data;
+      setProductsData(products);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  return (
+    <div>
+      <h1>ProductPage</h1>
+      {productsData &&
+        productsData.map((item, index) => {
+          return (
+            <div key={index}>
+              <li> {index + 1} </li>
+              <li> Name:{item?.name} </li>
+
+              <li> Description:{item?.description} </li>
+
+              <li>Price:{item?.price} </li>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
