f54ab09c5f30bb0ad76021ee4fdf1fb980bf4e45
Joseph Khella <josephkhella2010@gmail.com>
2025-07-27 11:57:22 +0200

    create slices

---
+++ frontend/clothes/src/SliceReducers/UserReducer.tsx
@@ -0,0 +1,21 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { UserType } from "../helps/InterfacesType";
+
+interface initialStateType {
+  userData: UserType[];
+}
+const initialState: initialStateType = {
+  userData: [],
+};
+
+const UserSlice = createSlice({
+  name: "UserSlice",
+  initialState,
+  reducers: {
+    setUserData: (state, action: PayloadAction<UserType[]>) => {
+      state.userData = action.payload;
+    },
+  },
+});
+export const { setUserData } = UserSlice.actions;
+export default UserSlice.reducer;
--- frontend/clothes/src/pages/Register/childComponent/RegisterSection.tsx
+++ frontend/clothes/src/pages/Register/childComponent/RegisterSection.tsx
@@ -1,21 +1,45 @@
 import { useState } from "react";
 import styles from "../registerPage.module.css";
 import type { UserType } from "../../../helps/InterfacesType";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../../../SliceReducers/UserReducer";
+import type { RootState } from "../../../store/Store";
 export default function RegisterSection() {
-  const [userData, setUserData] = useState<UserType>({
+  const { userData } = useSelector((state: RootState) => state.UserDataStore);
+
+  const dispatch = useDispatch();
+  const [userInfo, setuserInfo] = useState<UserType>({
     username: "",
     email: "",
     password: "",
   });
+  console.log(userData);
+  console.log("hi");
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/addUser",
+        userInfo
+      );
+      const { user } = response.data;
+      dispatch(setUserData(user));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className={styles.formContainer}>
-      <form action="" className={styles.formSection}>
+      <form action="" className={styles.formSection} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Username"
-          value={userData.username}
+          value={userInfo.username}
           onChange={(e) => {
-            setUserData((prev) => ({
+            setuserInfo((prev) => ({
               ...prev,
               username: e.target.value,
             }));
@@ -24,9 +48,9 @@ export default function RegisterSection() {
         <input
           type="text"
           placeholder="Email"
-          value={userData.email}
+          value={userInfo.email}
           onChange={(e) => {
-            setUserData((prev) => ({
+            setuserInfo((prev) => ({
               ...prev,
               email: e.target.value,
             }));
@@ -35,9 +59,9 @@ export default function RegisterSection() {
         <input
           type="text"
           placeholder="password"
-          value={userData.password}
+          value={userInfo.password}
           onChange={(e) => {
-            setUserData((prev) => ({
+            setuserInfo((prev) => ({
               ...prev,
               password: e.target.value,
             }));
