3199ca85e78b231ca5ec5fb1829b047f615c672b
Joseph Khella <josephkhella2010@gmail.com>
2025-07-28 12:31:24 +0200

    add new login code

--- frontend/clothes/src/pages/Login/childComponent/LoginSection.tsx
+++ frontend/clothes/src/pages/Login/childComponent/LoginSection.tsx
@@ -5,64 +5,27 @@ import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../../../store/Store";
 import { toast, ToastContainer } from "react-toastify";
 import axios from "axios";
-import { setUsersData } from "../../../SliceReducers/UserReducer";
+import {
+  setUsersData,
+  setToken,
+  setSingleUser,
+} from "../../../SliceReducers/UserReducer";
 export default function LoginSection() {
   const { userData } = useSelector((state: RootState) => state.UserDataStore);
-
+  const singleUserFromStorage = JSON.parse(
+    localStorage.getItem("user") || "{}"
+  );
   const dispatch = useDispatch();
   const [userInfo, setuserInfo] = useState<UserType>({
     username: "",
     password: "",
   });
-  /*    async function fetchUser() {
-    try {
-      const response = await axios.get(
-        "https://dg98ub8cgd.us-east-1.awsapprunner.com/api/users"
-      );
-      const { users } = response.data;
-      dispatch(setUsersData(users));
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
+  const tokenFromStorage = localStorage.getItem("token");
   useEffect(() => {
-    fetchUser();
-  }, [dispatch]);
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    if (!userInfo.username || !userInfo.email || !userInfo.password) {
-      toast.error("please fill all fields");
-      return;
+    if (tokenFromStorage) {
+      dispatch(setToken(tokenFromStorage));
     }
-    const existUser = userData.find(
-      (user) =>
-        user.email === userInfo.email || user.username === userInfo.username
-    );
-    if (existUser) {
-      toast.error("user is already exist");
-      return;
-    }
-
-    try {
-      const response = await axios.post(
-        "https://dg98ub8cgd.us-east-1.awsapprunner.com/api/addUser",
-        userInfo
-      );
-      const { user } = response.data;
-      dispatch(setUserData(user));
-      toast.success("User Registered Sucessfully");
-      setuserInfo({
-        username: "",
-        email: "",
-        password: "",
-      });
-      fetchUser();
-    } catch (error) {
-      console.log(error);
-    }
-  } */
-  /*  */
+  }, [dispatch, tokenFromStorage]);
   async function fetchUser() {
     try {
       const response = await axios.get(
@@ -86,8 +49,8 @@ export default function LoginSection() {
       (user) =>
         user.email === userInfo.email || user.username === userInfo.username
     );
-    if (existUser) {
-      toast.error("user is already exist");
+    if (!existUser) {
+      toast.error("user is not found");
       return;
     }
     if (!userInfo.username || !userInfo.password) {
@@ -95,15 +58,32 @@ export default function LoginSection() {
       return;
     }
     try {
+      const response = await axios.post(
+        "https://dg98ub8cgd.us-east-1.awsapprunner.com/api/loginUser",
+        userInfo
+      );
+      const { user, token } = response.data;
+      dispatch(setToken(token));
+      dispatch(setSingleUser(user));
+      toast.success("user os logged in sucessfully");
+      setuserInfo({
+        username: "",
+        password: "",
+      });
     } catch (error) {
       console.log(error);
     }
   }
   console.log(userData);
+  console.log(singleUserFromStorage, "single user");
+  console.log(tokenFromStorage);
 
   return (
     <div className={styles.formContainer}>
       <ToastContainer />
+      {singleUserFromStorage && (
+        <h2> welcome {singleUserFromStorage.username}</h2>
+      )}
       <form action="" className={styles.formSection} onSubmit={handleSubmit}>
         <input
           type="text"
